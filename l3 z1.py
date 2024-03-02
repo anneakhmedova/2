88a1e9376481eb64bb5c9fc14b0dd7129aa6a532
1 <anneakhmedova731@gmail.com>
2024-03-02 13:31:21 +0300

    Лабораторная работа №2 Задание 4

--- l3 z1.py
+++
@@ -1,8 +0,0 @@
-n=int(input('Введите количество строк:'))
-i=0
-b=''
-while i<n:
-    a=input('Введите слово:')
-    b=str(b)+' '+str(a)
-    i+=1
-print(b)
--- l3 z2.py
+++
@@ -1,7 +0,0 @@
-a=''
-b=''
-while a!='stop':
-    a=input('Введите слово:')
-    b=str(b)+' '+str(a)
-
-print(b.replace('stop',''))
